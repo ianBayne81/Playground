f0e1c38fbda8016a6483a11e0da06ae93cbde577
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2023-05-23 16:51:00 +1000

    updated html elements
    Added header element

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+// create header element
+const createHeader = document.createElement('h1')
+createHeader.textContent = "Read all about it!"
+const selectHeader = document.querySelector("#header")
+selectHeader.appendChild(createHeader)
 
+//create button / link element
 const createButton = document.createElement('button')
 createButton.textContent = ''
 const selectButtonElement = document.querySelector('#div-three')
