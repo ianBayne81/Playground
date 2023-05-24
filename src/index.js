eac004c13ec9752015c6e6c2b0a968c55ce50571
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2023-05-24 17:09:15 +1000

    Added time and date
    Added moment.js time and date

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ createHeader.textContent = "Read all about it!"
 const selectHeader = document.querySelector("#header")
 selectHeader.appendChild(createHeader)
 
+//create time and insert into header element
+const getDate = moment().format('MMMM Do YYYY, h:mm:ss a') // moment.js link from html page
+const createTime = document.createElement('p')
+createTime.textContent = getDate
+selectHeader.appendChild(createTime)
+
 //create button / link element
 const createButton = document.createElement('button')
 createButton.textContent = ''
-const selectButtonElement = document.querySelector('#div-three')
+const selectButtonElement = document.querySelector('#div-two')
 selectButtonElement.appendChild(createButton)
 createButton.setAttribute('id', 'jsButton')
 
@@ -15,6 +21,9 @@ document.querySelector('#jsButton').addEventListener('click', function (e) {
     e.preventDefault()
     location.assign('https://news.com.au')
    });
+
+
+
 
 
 
