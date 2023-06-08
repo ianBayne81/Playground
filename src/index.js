a3bab15adce4f05e4bee027c0c2c8fccadb03ae3
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2023-06-08 18:36:13 +1000

    Updated
    Incorporated GSAP into program to assist with animating letters on page load. And also moving div's on button click.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-// create header element
-const createHeader = document.createElement('h1')
-createHeader.textContent = "Read all about it!"
-const selectHeader = document.querySelector("#header")
-selectHeader.appendChild(createHeader)
+//GSAP code for plugin
+gsap.registerPlugin(Flip)
 
 //create time and insert into header element
-const getDate = moment().format('MMMM Do YYYY, h:mm:ss a') // moment.js link from html page
+const getDate = moment().format('LLL') // moment.js link from html page
 const createTime = document.createElement('p')
 createTime.textContent = getDate
+const selectHeader = document.querySelector('#header')
 selectHeader.appendChild(createTime)
 
 //create button / link element
 const createButton = document.createElement('button')
 createButton.textContent = ''
-const selectButtonElement = document.querySelector('#div-two')
+const selectButtonElement = document.querySelector('#button-element')
 selectButtonElement.appendChild(createButton)
 createButton.setAttribute('id', 'jsButton')
 
@@ -22,7 +20,35 @@ document.querySelector('#jsButton').addEventListener('click', function (e) {
     location.assign('https://news.com.au')
    });
 
+//code for moving letters with flip on page load
 
+const targetLetterI = document.querySelector('#i')
+const targetLetterA = document.querySelector('#a')
+const targetLetterN = document.querySelector('#n')
+window.onload = function () {
+    let letterI = Flip.getState('#i')
+    let letterA = Flip.getState('#a')
+    let letterN = Flip.getState('#n')
+    selectHeader.appendChild(targetLetterI)
+    selectHeader.appendChild(targetLetterA)
+    selectHeader.appendChild(targetLetterN)
+    Flip.from(letterI, {duration: 3, ease: "power1.out"})
+    Flip.from(letterA, {duration: 4, ease: "power1.out"})
+    Flip.from(letterN, {duration: 5, ease:"power1.out"})
+}
+
+//code for moving box and button with flip when page is clicked
+const container = document.querySelector('#container')
+const box = document.querySelector('#box')
+
+document.addEventListener('click', (e) => {
+    let boxState = Flip.getState('#box')
+    let buttonState = Flip.getState('#jsButton')
+    container.appendChild(box)
+    box.appendChild(selectButtonElement)
+    Flip.from(boxState, { duration: 3, ease: "power1.out"})
+    Flip.from(buttonState, { duration: 5, ease: "power1.out"})
+})
 
 
 
