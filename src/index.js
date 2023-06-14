36f5914c731a30f52772e500000a26cf55e60861
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2023-06-14 17:24:16 +1000

    Changed design of project
    Project is now a calculator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-//GSAP code for plugin
-gsap.registerPlugin(Flip)
+
 
 //create time and insert into header element
 const getDate = moment().format('LLL') // moment.js link from html page
@@ -8,48 +7,6 @@ createTime.textContent = getDate
 const selectHeader = document.querySelector('#get-date')
 selectHeader.appendChild(createTime)
 
-//create button / link element
-const createButton = document.createElement('button')
-createButton.textContent = ''
-const selectButtonElement = document.querySelector('#button-element')
-selectButtonElement.appendChild(createButton)
-createButton.setAttribute('id', 'jsButton')
-
-document.querySelector('#jsButton').addEventListener('click', function (e) {
-    e.preventDefault()
-    location.assign('https://news.com.au')
-   });
-
-//code for moving letters with flip on page load
-
-const targetLetterI = document.querySelector('#i')
-const targetLetterA = document.querySelector('#a')
-const targetLetterN = document.querySelector('#n')
-const letterHome = document.querySelector('#letters-home')
-window.onload = function () {
-    let letterI = Flip.getState('#i')
-    let letterA = Flip.getState('#a')
-    let letterN = Flip.getState('#n')
-    letterHome.appendChild(targetLetterI)
-    letterHome.appendChild(targetLetterA)
-    letterHome.appendChild(targetLetterN)
-    Flip.from(letterI, {duration: 3, ease: "power1.out"})
-    Flip.from(letterA, {duration: 4, ease: "power1.out"})
-    Flip.from(letterN, {duration: 5, ease:"power1.out"})
-}
-
-//code for moving box and button with flip when page is clicked
-const container = document.querySelector('#container')
-const box = document.querySelector('#box')
-
-document.addEventListener('click', (e) => {
-    let boxState = Flip.getState('#box')
-    let buttonState = Flip.getState('#jsButton')
-    container.appendChild(box)
-    box.appendChild(selectButtonElement)
-    Flip.from(boxState, { duration: 3, ease: "power1.out"})
-    Flip.from(buttonState, { duration: 5, ease: "power1.out"})
-})
 
 
 
